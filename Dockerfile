d3af9209467a549959a7a2e53a1c547f0f09e22b
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2024-06-12 23:20:42 -0300

    chore: Update Dockerfile and action.yml

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,7 @@ FROM driftive/driftive:0.2 as build
 FROM alpine:3.12
 COPY --from=build /usr/local/bin/driftive /usr/local/bin/driftive
 COPY entrypoint.sh /entrypoint.sh
+
+RUN apk add tenv --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,19 @@
 REPO_PATH=./
 SLACK_URL=$1
 CONCURRENCY=$2
+TERRAFORM_DISTRIBUTION=$3
+TERRAFORM_DISTRIBUTION_VERSION=$4
+
+if [ "$TERRAFORM_DISTRIBUTION" != "tf" ] && [ "$TERRAFORM_DISTRIBUTION" != "tofu" ]; then
+    echo "Invalid value for TERRAFORM_DISTRIBUTION"
+    exit 1
+fi
 
 if [ -z "SLACK_URL" ]; then
   echo "SLACK_URL is required"
   exit 1
 fi
+
+tenv install $TERRAFORM_DISTRIBUTION $TERRAFORM_DISTRIBUTION_VERSION
 
 driftive --repo-path="$REPO_PATH" --slack-url="$SLACK_URL" --concurrency="$CONCURRENCY"
