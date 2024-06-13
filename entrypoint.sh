7ebf14f9e643d695f87e76f419ebd3a23fd2be5b
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2024-06-12 22:58:48 -0300

    fix: Add validation for SLACK_URL in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,9 @@ REPO_PATH=./
 SLACK_URL=$1
 CONCURRENCY=$2
 
-driftive --repo-path=$REPO_PATH --slack-url=$SLACK_URL --concurrency=$CONCURRENCY
+if [ -z "SLACK_URL" ]; then
+  echo "SLACK_URL is required"
+  exit 1
+fi
+
+driftive --repo-path="$REPO_PATH" --slack-url="$SLACK_URL" --concurrency="$CONCURRENCY"
