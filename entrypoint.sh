211f35384bc154ac39003ae93d010f6c440e1dee
Fernando Nogueira <fernandonogueira@users.noreply.github.com>
2025-04-04 21:07:47 -0300

    chore(entrypoint): enhance installation checks for Terraform and Terragrunt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,37 @@
 #!/bin/sh -l
+set -ex
 
 if [ "$TERRAFORM_DISTRIBUTION" != "tf" ] && [ "$TERRAFORM_DISTRIBUTION" != "tofu" ]; then
     echo "Invalid value for TERRAFORM_DISTRIBUTION"
     exit 1
 fi
 
+# Install TF distribution
 tenv $TERRAFORM_DISTRIBUTION install $TERRAFORM_DISTRIBUTION_VERSION
+
+# Check if the installation was successful
+if [ "$TERRAFORM_DISTRIBUTION" = "tf" ]; then
+  # Check if terraform is installed
+  if ! command -v terraform &> /dev/null; then
+    echo "Terraform could not be found"
+    exit 1
+  fi
+elif [ "$TERRAFORM_DISTRIBUTION" = "tofu" ]; then
+  # Check if tofu is installed
+  if ! command -v tofu &> /dev/null; then
+    echo "Tofu could not be found"
+    exit 1
+  fi
+fi
 
 # terragrunt is optional. If present, install it using tenv
 if [ -n "$TERRAGRUNT_VERSION" ]; then
   tenv tg install $TERRAGRUNT_VERSION
+  # Check if terragrunt is installed
+  if ! command -v terragrunt &> /dev/null; then
+    echo "Terragrunt could not be found"
+    exit 1
+  fi
 fi
 
 driftive_args=" --repo-path=./"
